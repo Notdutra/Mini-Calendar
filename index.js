8d148d5ef4984ae00e57dd72962de9e29f3a29d3
Arthur S Dutra <arthurdutra@live.ca>
2024-05-29 17:03:35 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const monthNameEl = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumberEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+
+monthNameEl.innerHTML = date.toLocaleString("en", {
+    month: "long"
+})
+
+dayNameEl.innerHTML = date.toLocaleString("en", {
+    weekday: "long"
+})
+
+dayNumberEl.innerHTML = date.getDate();
+
+yearEl.innerHTML = date.getFullYear();
+
